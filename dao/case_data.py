2b7a4edf3ffaa4ed18316d0442fc36c802a3fe89
AssKickerSurfaceBook <121584565@163.com>
2018-12-14 17:06:24 +0800

    init case data

---
+++ dao/case_data.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+from dao.graph_data import GraphData
+
+
+class CaseData:
+    table = MongoClient('101.132.40.25', 27017).get_database('judging').get_collection('case')
+
+    def __init__(self, d: dict, case_id: str):
+        self.d = d
+        self.case_id = case_id
+        self.d['_id'] = case_id
+
+    @staticmethod
+    def create_case(graph_name: str, case_id: str) -> 'CaseData':
+        d = GraphData().fetch(graph_name)
+        return CaseData(d, case_id) if d is not None else None
+
+    @staticmethod
+    def fetch_case(case_id: str) -> 'CaseData':
+        d = CaseData.table.find_one({'_id': case_id})
+        return CaseData(d, case_id) if d is not None else None
+
+    
