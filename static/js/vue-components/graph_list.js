b28afe4a15cf68fa8fa0fc4b2938d20b6d5cf03c
DoctorBear <2948363603@qq.com>
2019-01-05 16:36:39 +0800

    complete uploading file privately

--- static/js/vue-components/graph_list.js
+++ static/js/vue-components/graph_list.js
@@ -1,35 +1,46 @@
 Vue.component('graph-list', {
-    props: ['leaveToClass', 'active', 'carouselList'],
+    props: ['graphList'],
+    data:function(){
+        return{
+            isNewCase: false
+        }
+    },
     template: `
-    <div class="graph-list-box">
-                    <ul class="graph-list">
-                        <li class="graph-item" v-for="(item, index) in graphlist">
-                            <div class="item-top">
-                                <span class="item-text">{{ item }}</span>
-                                <div class="item-button-box">
-                                    <a class="item-button glyphicon glyphicon-plus" v-on:click="newCase()"></a>
-                                    <a class="item-button glyphicon glyphicon-minus"></a>
-                                </div>
-                            </div>
-                            <div class="item-bottom" :class="[ isNewCase ? 'show-input' : 'hide-input']">
-                                <input id="case-id-input" class="case-input" type="text" >
-                                <div class="item-button-box">
-                                    <a class="item-button glyphicon glyphicon-ok"></a>
-                                    <a class="item-button glyphicon glyphicon-remove" v-on:click="newCase()"></a>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
+        <div class="graph-list-box">
+            <ul class="graph-list">
+                <li class="graph-item" v-for="(item, index) in graphList">
+                    <div class="item-top">
+                        <span class="item-text">{{ item }}</span>
+                        <div class="item-button-box">
+                            <a class="item-button glyphicon glyphicon-plus" @click="newCase()"></a>
+                            <a class="item-button glyphicon glyphicon-minus"></a>
+                        </div>
+                    </div>
+                    <div class="item-bottom" :class="[ isNewCase ? 'show-input' : 'hide-input']">
+                        <input id="case-id-input" class="case-input" type="text" ref="caseName">
+                        <div class="item-button-box">
+                            <a class="item-button glyphicon glyphicon-ok" @click="createCase()"></a>
+                            <a class="item-button glyphicon glyphicon-remove" @click="newCase()"></a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
     `,
 
     methods: {
-        move: function (offset) {
-            this.$emit('carousel-move', offset);
+        move: function(graph_name){
+            this.$emit('show-cases', graph_name)
         },
-
-        ocr: function (absPath) {
-            this.$emit('ocr', absPath)
+        newCase: function (event) {
+            if(this.isNewCase){
+                this.isNewCase = false;
+            }else{
+                this.isNewCase = true;
+            }
+        },
+        createCase: function(event) {
+            console.log(this.$refs.caseName.text)
         }
     }
 });
