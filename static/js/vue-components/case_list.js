ce3d6eabc0b3f71861b1dc8b50c39d155234442c
DoctorBear <2948363603@qq.com>
2019-01-06 23:12:41 +0800

    complete uploading file privately

---
+++ static/js/vue-components/case_list.js
@@ -0,0 +1,56 @@
+Vue.component('case-list', {
+    props: ['caseList'],
+    data:function (){
+            return{
+                currentIndex:"-1",
+                inputList:[],
+
+                searchCase:""
+            }
+        },
+    computed: {
+        cases: function () {
+            var result = Array();
+            var length = this.searchCase.length
+            for (var i = 0; i < this.caseList.length; i++) {
+                if(this.caseList[i].length >= length && this.caseList[i].substring(0,length)==this.searchCase) {
+                    result.push(this.caseList[i]);
+                }
+            }
+            return result;
+        }
+    },
+    template: `
+        <div>
+            <div class="search-box">
+                <input id="search_input" class="search" type="text" v-model="searchCase">
+                <span class="search-icon glyphicon glyphicon-search"></span>
+            </div>
+            <hr class="separate-line"/>
+            <div class="graph-list-box">
+                <ul class="graph-list">
+                    <li class="graph-item" v-for="(item, index) in this.cases" :id="index">
+                        <div class="item-top ">
+                            <span class="item-text" @click="changeCases(item)">{{ item }}</span>
+                            <div class="item-button-box">
+                                <a class="item-button glyphicon glyphicon-cog" @click="configCase(item)"></a>
+                                <a class="item-button glyphicon glyphicon-minus" @click="removeCase(item)"></a>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    `,
+
+    methods: {
+        configCase: function(item) {
+            document.cookie="case-id="+item;
+            const url = "/config-graph";
+            window.location.href = url
+        },
+        removeCase: function(item) {
+            this.$emit('remove-case', item)
+        },
+    }
+});
